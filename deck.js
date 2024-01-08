8f548dd25d88aa61536bcfb66506ea26e9bdbf2f
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-01-08 17:49:59 -0600

    Rewrote functions that create and shuffle the deck, which is now an object array

--- deck.js
+++ deck.js
@@ -1,66 +1,23 @@
-// Establish all cards in a standard deck of playing cards
-const deck = [
-    ["2", "clubs"],
-    ["3", "clubs"],
-    ["4", "clubs"],
-    ["5", "clubs"],
-    ["6", "clubs"],
-    ["7", "clubs"],
-    ["8", "clubs"],
-    ["9", "clubs"],
-    ["10", "clubs"],
-    ["J", "clubs"],
-    ["Q", "clubs"],
-    ["K", "clubs"],
-    ["A", "clubs"],
-    ["2", "diamonds"],
-    ["3", "diamonds"],
-    ["4", "diamonds"],
-    ["5", "diamonds"],
-    ["6", "diamonds"],
-    ["7", "diamonds"],
-    ["8", "diamonds"],
-    ["9", "diamonds"],
-    ["10", "diamonds"],
-    ["J", "diamonds"],
-    ["Q", "diamonds"],
-    ["K", "diamonds"],
-    ["A", "diamonds"],
-    ["2", "hearts"],
-    ["3", "hearts"],
-    ["4", "hearts"],
-    ["5", "hearts"],
-    ["6", "hearts"],
-    ["7", "hearts"],
-    ["8", "hearts"],
-    ["9", "hearts"],
-    ["10", "hearts"],
-    ["J", "hearts"],
-    ["Q", "hearts"],
-    ["K", "hearts"],
-    ["A", "hearts"],
-    ["2", "spades"],
-    ["3", "spades"],
-    ["4", "spades"],
-    ["5", "spades"],
-    ["6", "spades"],
-    ["7", "spades"],
-    ["8", "spades"],
-    ["9", "spades"],
-    ["10", "spades"],
-    ["J", "spades"],
-    ["Q", "spades"],
-    ["K", "spades"],
-    ["A", "spades"]
-];
+// Initializes the deck as an empty array
+const deck = [];
 
-//Prints the contents of deck to the console
-function deckContents() {
+// Establishes suits and values of cards in a standard 52-card deck
+const suits = ["clubs", "diamonds", "hearts", "spades"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+// Creates a new deck from the suits and values arrays and prints the deck's contents to the console
+function createDeck() {
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            let card = {Value: values[j], Suit: suits[i]};
+            deck.push(card);
+        }
+    }
     console.log(deck); 
 }
 
-//Shuffles the deck using the Fisher-Yates Algorithm and prints to the console
-function shuffle(deck) {
+// Shuffles the deck using the Fisher-Yates Algorithm and prints to the console
+function shuffle() {
     for (let i = deck.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [deck[i], deck[j]] = [deck[j], deck[i]];
