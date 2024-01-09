2b65ae07aa425c7383474b8217651e1cacc547c3
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-01-08 19:00:47 -0600

    Added photos for all 52 cards

--- deck.js
+++ deck.js
@@ -15,9 +15,9 @@ function shuffle() {
         }
     }
     // Deck shuffling
-    for (let i = deck.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
+    // for (let i = deck.length - 1; i > 0; i--) {
+    //   let j = Math.floor(Math.random() * (i + 1));
+    //   [deck[i], deck[j]] = [deck[j], deck[i]];
+    // }
     console.log(deck); 
 }
