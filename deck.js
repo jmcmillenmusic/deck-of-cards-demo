a80f81e49b767d728f4484934a5074430d581ad7
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-01-08 18:03:18 -0600

    Simplified deck creation and shuffling

--- deck.js
+++ deck.js
@@ -5,19 +5,16 @@ const deck = [];
 const suits = ["clubs", "diamonds", "hearts", "spades"];
 const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
-// Creates a new deck from the suits and values arrays and prints the deck's contents to the console
-function createDeck() {
+// Creates and shuffles the deck using the Fisher-Yates Algorithm and prints to the console
+function shuffle() {
+    // Deck creation
     for (let i = 0; i < suits.length; i++) {
         for (let j = 0; j < values.length; j++) {
             let card = {Value: values[j], Suit: suits[i]};
             deck.push(card);
         }
     }
-    console.log(deck); 
-}
-
-// Shuffles the deck using the Fisher-Yates Algorithm and prints to the console
-function shuffle() {
+    // Deck shuffling
     for (let i = deck.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [deck[i], deck[j]] = [deck[j], deck[i]];
